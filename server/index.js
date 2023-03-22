015ac57d369f6b2bcf6865c9830959890029fae5
ma3ti <matteo.fontana9@libero.it>
2023-03-22 15:28:31 +0100

    add mongo.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const portNumber = '5000';
 
 let app = express();
 
-
+const uri = "mongodb+srv://admin:ulUpCKpLJQ763Mvx@cluster0.r94j7ya.mongodb.net/?retryWrites=true&w=majority";
 
 
 app.get('/', (req, res) => {
@@ -10,13 +13,22 @@ app.get('/', (req, res) => {
 });
 
 
+async function connect(){
+    try {
+        await mongoose.connect(uri);
+        console.log('Connected to Db');
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
 
 
+connect();
 
 
-
-
-
-app.listen('5000', () => {
-    console.log('Server is listening on port 5000 ...');
+app.listen(portNumber, () => {
+    global.startDate = new Date() ; 
+	console.log(`App listening on port ${portNumber} started ${global.startDate.toLocaleString()}`);
 })
