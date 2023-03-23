aba6d8cfe19ef4812ae7594881a93a9770573aa4
ma3ti <matteo.fontana9@libero.it>
2023-03-23 11:48:06 +0100

    express route and mongoose connect

--- server/index.js
+++ server/index.js
@@ -1,42 +1,35 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
-
+const routes = require('./routes/routes');
+require('dotenv').config();
 
 const PORT = '5000';
+const mongoUrl = process.env.MONGO_URL;
 
+//  MongoDB connection
+mongoose.connect(mongoUrl);
+const database = mongoose.connection;
 
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+// express app
 let app = express();
 
+app.use(express.json());
+app.use('/api', routes);
 
-
-
-/*
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '.../.../src/public/views/homepage.html');
-});*/
-
- 
 app.get('/', function (req, res) {
     res.send('<h1> Hello World !!! </h1>');
 });
 
 
 
-//  MongoDb Connection
-/*
-async function connect(){
-    try {
-        await mongoose.connect(uri);
-        console.log('Connected to Db');
-        
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-connect();
-*/
 
 app.listen(PORT, () => {
     global.startDate = new Date() ; 
